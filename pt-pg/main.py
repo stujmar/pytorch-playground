aeb79ada534787bd3a24c616b1068cc06d4be65f
Stuart Marsh <stujmar@gmail.com>
2023-01-27 20:27:47 -0600

    [sm] brought in requests and pulled in a call to check status code

--- pt-pg/main.py
+++ pt-pg/main.py
@@ -1,8 +1,19 @@
 import torch
+import requests
 
 def main():
     '''This is the main function.'''
-    print(dir(torch))
+    # Examime the torch module
+    # print(dir(torch))
+
+    # Get data from the web
+    get_data()
+
+def get_data():
+    '''This function gets data from the web.'''
+    url = 'https://www.google.com'
+    response = requests.get(url)
+    print(response.status_code)
 
 if __name__ == '__main__':
     main()
